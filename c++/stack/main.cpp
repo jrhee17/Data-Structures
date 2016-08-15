e549548ccec3934249e907f82d685eb3cd379331
John <john@Johns-MacBook-Pro.local>
2016-08-15 22:17:46 +0900

    Stack implementation complete

--- c++/stack/main.cpp
+++ c++/stack/main.cpp
@@ -8,66 +8,127 @@ using namespace std;
 class Stack{
     public:
         Stack();
+        Stack(int size);
+        ~Stack();
+
         void push(int value);
         int pop();
         int size();
         void print();
         bool empty();
+
     private:
         vector<int> vec;
+        int stack_size;
+        int* array;
+
+        int start_ind;
+        int end_ind;
 };
 
 Stack::Stack(void) {
+    this->stack_size = 100;
+    this->array = new int[this->stack_size];
+    this->start_ind = 0;
+    this->end_ind = 0;
+}
 
+Stack::Stack(int stack_size) {
+    this->stack_size = stack_size;
+    this->array = new int[stack_size];
+    this->start_ind = 0;
+    this->end_ind = 0;
+}
+
+Stack::~Stack() {
+    delete this->array;
 }
 
 void Stack::push(int value) {
-    vec.push_back(value);
+    
+    // Check if index array is full
+    if(size() == this->stack_size) {
 
-    print();
+        // Create a new stack of size multiplied by 2
+        int *before_address = &(*this->array);
+        int before_stack_size = this->stack_size;
+
+        this->stack_size *= 2;
+        int* new_array = new int[this->stack_size];
+        copy(this->array, this->array + this->end_ind, new_array);
+        delete[] this->array;
+        this->array = new_array;
+
+        int *after_address = &(*this->array);
+        int after_stack_size = this->stack_size;
+
+        cout << "Increasing array length: " << before_stack_size << " (" << before_address << ") to " << after_stack_size << " ("<< after_address << ")" << endl;
+    }
+
+    this->array[this->end_ind] = value;
+    this->end_ind++;
+
 }
 
 int Stack::pop() {
     if(empty())
         throw runtime_error("Underflow Error"); 
 
-    int value = vec.back();
-    vec.pop_back();
+    // Check if size is decreased enough to reduce size
+    if(this->stack_size / 2 == size() && this->stack_size > 1) {
 
-    print();
+        int *before_address = &(*this->array);
+        int before_stack_size = this->stack_size;
+
+        // Create a new stack of size divided by 2
+        this->stack_size /= 2;
+        int* new_array = new int[this->stack_size];
+        copy(this->array, this->array + this->end_ind, new_array);
+        delete[] this->array;
+        this->array = new_array;
+
+        int *after_address = &(*this->array);
+        int after_stack_size = this->stack_size;
+
+        cout << "Decreasing array length: " << before_stack_size << " (" << before_address << ") to " << after_stack_size << " ("<< after_address << ")" << endl;
+    }
+
+    int value = this->array[this->end_ind];
+    this->end_ind--;
 
     return value;
 }
 
 int Stack::size() {
-    return vec.size();
+    return this->end_ind - this->start_ind;
 }
 
 bool Stack::empty() {
-    if (vec.size() == 0)
+    if (size() == 0)
         return true;
     return false;
 }
 
 void Stack::print() {
-    for(int i = 0; i < vec.size(); i++) {
-        cout << vec[i] << " ";
-    }
+    for(int i = this->start_ind; i < this->end_ind; i++)
+        cout << *(this->array + i) << " ";
     cout << endl;
 }
 
 int main() {
 
-    Stack stack;
-    stack.push(1);
-    stack.push(3);
-    stack.pop();
-    stack.push(5);
-    stack.push(-1);
-    stack.pop();
-    stack.pop();
-    stack.pop();
-    stack.pop();
+    cout << "Pushing 20 values and popping 30 values..." << endl;
+
+    Stack stack(5);
+    for(int i = 0; i < 20; i++) {
+        stack.push(i);
+        stack.print();
+    }
+
+    for(int i = 0; i < 30; i++) {
+        stack.pop();
+        stack.print();
+    }
 
     return 1;
 }
